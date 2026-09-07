b09a5ea2899efbd26c28acd13090bcab138fa61d
gabriel-thuus <gabriel.thuus@epitech.eu>
2026-09-07 16:36:34 +0900

    chore(11): rename id variable from id_ to _id

--- src/ECS/Entity.hpp
+++ src/ECS/Entity.hpp
@@ -9,16 +9,16 @@ namespace ECS {
     class Entity {
         public:
             // construction must be explicit (no implicit conversion from size_t)
-            explicit Entity(std::size_t id) noexcept : id_(id) {}
+            explicit Entity(std::size_t id) noexcept : _id(id) {}
 
             // optionally allow default construction (identity 0). Remove if you want to force explicit init.
             Entity() noexcept = default;
 
             // implicitly convertible to size_t
-            operator std::size_t() const noexcept { return id_; }
+            operator std::size_t() const noexcept { return _id; }
 
             // explicit accessor
-            std::size_t value() const noexcept { return id_; }
+            std::size_t value() const noexcept { return _id; }
 
             // Name is added to be able to display the entity in the UI only
             void setName(const std::string &name) { _name = name; };
@@ -26,11 +26,11 @@ namespace ECS {
 
             // Comparisons use the identity only; the name is display metadata.
             std::strong_ordering operator<=>(const Entity& other) const noexcept {
-                return id_ <=> other.id_;
+                return _id <=> other._id;
             }
 
         private:
-            std::size_t id_{0};
+            std::size_t _id{0};
             std::string _name="";
     };
 }
